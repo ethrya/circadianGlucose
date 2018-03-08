279af871d4e0d95f76e9298717c415b23aa883b0
Ethan Ryan <erya7975@uni.sydney.edu.au>
2018-03-08 17:12:32 +1100

    converted solvers to functions. Added comparison code and phase planes

--- liSolver.m
+++ liSolver.m
@@ -1,22 +1,16 @@
+function sol=liSolver(state, const, time)
 %Script to solve the Li et al. (2006) two delay DDE model
 
-const = models.constants;
-const.Gin = 135;
-const.tau1 = 7;
-const.tau2 = 36;
+% const = models.constants;
+% const.Gin = 136;
+% const.tau1 = 7;
+% const.tau2 = 36;
 
-% Initial conditions
-state = [10500; % Glucose
-         30]; % Insulin
+% % Initial conditions
+% state = [10500; % Glucose
+%          30]; % Insulin
 
 % Solve equations
 opts = odeset('RelTol',1e-6);
-sol = dde23(@(t,y,z) models.Li(t,y,z,const), [const.tau1, const.tau2], state, [0,600]);
-
-subplot(2,1,1)
-plot(sol.x, sol.y(2,:)/const.Vp)
-ylabel('Insulin (\muU/ml)')
-subplot(2,1,2)
-plot(sol.x, sol.y(1,:)/(10*const.Vg));
-xlabel('Time (min)')
-ylabel('Glucose (mg/dl)')
+sol = dde23(@(t,y,z) models.Li(t,y,z,const), [const.tau1, const.tau2], state, time, opts);
+end
--- sturisSolver.m
+++ sturisSolver.m
@@ -1,35 +1,8 @@
 % Program to simulate the Sturis et al. 1991 model
-
-%% Preliminaries
-% Import constants file
-const = models.constants;
-
-% Change constants here
-const.td = 36;
-const.Gin = 216;
-
-% Create initial condition
-state = [30; % Ip
-         0; % Ii
-         13000; % G
-         0; % x1
-         0; % x2
-         0]; % x3
+function [t, y] = sturisSolver(state, const, time)
 
 %% Solve ODE using stiff solver
-[t, y] = ode15s(@(t,y) models.sturis(t, y, const), [0,1400], state);
+opts = odeset('RelTol',1e-6);
+[t, y] = ode15s(@(t,y) models.sturis(t, y, const), time, state, opts);
 
-
-%% Plot output
-% Convert values to concentrations
-Ip = y(:,1)/const.Vp; %[I]=I/Vp microU/ml
-G = y(:,3)/(const.Vg*10); %[G]=G/Vg mg/dl
-
-subplot(2,1,1)
-plot(t,Ip)
-axis([0, 1400, 0, 50])
-ylabel('Insulin (\muU/ml)')
-subplot(2,1,2)
-plot(t,G)
-xlabel('Time (min)')
-ylabel('Glucose (mg/dl)')
+end
