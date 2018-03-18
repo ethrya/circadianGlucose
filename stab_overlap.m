df8be2d1744498b5c75787582e55b19b9e258df3
Ethan Ryan <ethanryan9@gmail.com>
2018-03-18 18:46:19 +1100

    Added code to plot overlaping regions of Li, Sturis, and Tolic model stability

---
+++ stab_overlap.m
@@ -0,0 +1,45 @@
+% Code to create plot of locations where models are stable/limit cycles
+
+% Load data
+data = load('../circadian_results/bifur04');
+
+liStab = abs(data.LiResults(:,:,1)-data.LiResults(:,:,2))<1;
+sturisStab = abs(data.SturisResults(:,:,1)-data.SturisResults(:,:,2))<1;
+tolicStab = abs(data.TolicResults(:,:,1)-data.TolicResults(:,:,2))<1;
+
+[Tau,Gin]=meshgrid(data.tau, data.Gin);
+
+stabMatrix = zeros(length(liStab(:,1)),length(liStab(1,:)));
+
+for i=1:length(liStab(:,1))
+    for j=1:length(liStab(1,:))
+
+        if liStab(i,j)==0 && sturisStab(i,j)==0 && tolicStab(i,j)==0
+            stabMatrix(i,j) = 0;
+        elseif liStab(i,j)==0 && sturisStab(i,j)==0 && tolicStab(i,j)==0
+            stabMatrix(i,j) = 1;
+        elseif liStab(i,j)==0 && sturisStab(i,j)==1 && tolicStab(i,j)==0
+            stabMatrix(i,j) = 2;
+        elseif liStab(i,j)==0 && sturisStab(i,j)==0 && tolicStab(i,j)==1
+            stabMatrix(i,j) = 3;
+        elseif liStab(i,j)==0 && sturisStab(i,j)==0 && tolicStab(i,j)==1
+            stabMatrix(i,j) = 4;
+        elseif liStab(i,j)==1 && sturisStab(i,j)==1 && tolicStab(i,j)==0
+            stabMatrix(i,j) = 5;
+        elseif liStab(i,j)==0 && sturisStab(i,j)==1 && tolicStab(i,j)==1
+            stabMatrix(i,j) = 6;
+        elseif liStab(i,j)==1 && sturisStab(i,j)==1 && tolicStab(i,j)==1
+            stabMatrix(i,j) = 7;
+        end
+    end
+end
+
+save('../circadian_results/stabMat', 'stabMatrix');
+
+figure
+mesh(Tau,Gin,stabMatrix);
+
+
+
+
+
