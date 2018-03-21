7c43f5a558b17172ef0b4da69b4c9d41cf45afc0
Ethan Ryan <erya7975@uni.sydney.edu.au>
2018-03-21 11:41:48 +1100

    Fixed bug which resulted in the wrong stabilities being calculated in Sturis and Tolic models due to incorrect array indicesw

--- stab_location_plot.m
+++ stab_location_plot.m
@@ -1,14 +1,18 @@
 % Code to create plot of locations where models are stable/limit cycles
 
 % Load data
-data = load('~/results/bifur03');
+data = load('~/results/bifur04');
 
 liStab = abs(data.LiResults(:,:,1)-data.LiResults(:,:,2))<1;
-sturisStab = data.SturisResults(:,1,1)-data.SturisResults(:,2,1)<1e-4;
-tolicStab = data.TolicResults(:,1,1)-data.TolicResults(:,2,1)<1e-4;
+sturisStab = abs(data.SturisResults(:,:,1)-data.SturisResults(:,:,2))<1;
+tolicStab = abs(data.TolicResults(:,:,1)-data.TolicResults(:,:,2))<1;
 
 [Tau,Gin]=meshgrid(data.tau, data.Gin);
 
 figure
 mesh(Tau,Gin,liStab);
+figure
+mesh(Tau, Gin, sturisStab);
+figure
+mesh(Tau, Gin, tolicStab);
 %set(gca, 'XTick', 1:5, 'XTickLabel', data.tau);
