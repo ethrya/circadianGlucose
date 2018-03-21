3093c352a86c43edc2fd4a590e88726c3682f330
Ethan Ryan <erya7975@uni.sydney.edu.au>
2018-03-21 11:37:25 +1100

    Bug fixes and output changes Fixed a bug which resulted in a wrong tolicResults matrix beging created. Added a beggining simulation print out to before the for loop.

--- bifurification_gin.m
+++ bifurification_gin.m
@@ -1,20 +1,21 @@
+clear all;
 tic
 % Bifurification parameter
-Gin = 0:5:300;
+Gin = 50:2:200;
 nValues = length(Gin);
 
 %Constants for all trials
 const = models.constants;
 const.tau1 = 7;
-const.tau2 = 36;
-const.td = 36;
+const.tau2 = 50;
+const.td = 50;
 
 constArray(1:nValues) = const;
 
 % Initial conditions
 % Li et al
 liState = [13000; % Glucose
-         30]; % Insulin
+           30]; % Insulin
 % Sturis et al. and Tolic et al conditions
 sturisState = [30; % Ip
                0; % Ii
@@ -31,17 +32,22 @@ tmin = 0.9*time(2);
 % Form: min G, max G, min I, max I
 LiResults = zeros(nValues, 4);
 SturisResults = zeros(nValues, 4);
-tolicResults = zeros(nValues, 4);
+TolicResults = zeros(nValues, 4);
+
+fprintf('Beginning Simulations');
+
 
 % Store Values for each parameter
 parfor i=1:nValues
+    fprintf('Completed %i of %i Gin Values \n', i, nValues);
+
     % Update Gin value
     constArray(i).Gin = Gin(i);
     
     % Solve Equations
     sol = liSolver(liState, constArray(i), time);
     [t, y] = sturisSolver(sturisState, constArray(i), time);
-    [tT, yT] = tolicSolver(sturisState, const, time);
+    [tT, yT] = tolicSolver(sturisState, constArray(i), time);
     
     % Store Results
     LiResults(i,:) = [min(sol.y(1,sol.x>tmin)), max(sol.y(1,sol.x>tmin))...
@@ -52,10 +58,16 @@ parfor i=1:nValues
                          min(yT(tT>tmin,1)), max(yT(tT>tmin,1))];
 end
 
-
 toc
 
-%%
+%% Experimental results (Gin, min, max)
+amplitude = [200 11200 7400; % Simon 87
+             300 10800 18000; % Shapiro 88 - 1 subject
+             243 10000 13000]; % Van Cauter 89 1 subject
+
+
+%% 
+subplot(2,1,1)
 hold on
 plot(Gin, LiResults(:,1), 'b')
 plot(Gin, LiResults(:,2), 'b')
@@ -63,4 +75,20 @@ plot(Gin, SturisResults(:,1), 'r')
 plot(Gin, SturisResults(:,2), 'r')
 plot(Gin, TolicResults(:,1), 'y')
 plot(Gin, TolicResults(:,2), 'y')
+plot(amplitude(:,1), amplitude(:,2),'kx')
+plot(amplitude(:,1), amplitude(:,3),'kx')
+xlabel('Gin (mg/min)')
+ylabel('G_p (mg)')
+hold off
+
+subplot(2,1,2)
+hold on
+plot(Gin, LiResults(:,3), 'b')
+plot(Gin, LiResults(:,4), 'b')
+plot(Gin, SturisResults(:,3), 'r')
+plot(Gin, SturisResults(:,4), 'r')
+plot(Gin, TolicResults(:,3), 'y')
+plot(Gin, TolicResults(:,4), 'y')
+xlabel('Gin (mg/min)')
+ylabel('I_p (\mu U)')
 hold off
