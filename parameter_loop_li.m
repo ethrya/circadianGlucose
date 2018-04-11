1ed4013bc411b5c89c04e9daf4c635c77624b423
Ethan Ryan <erya7975@uni.sydney.edu.au>
2018-04-11 16:35:46 +1000

    Record and plot baseline [G] for each parameter.

--- parameter_loop_li.m
+++ parameter_loop_li.m
@@ -1,12 +1,12 @@
 clear all;
 tic()
 %% Preliminaries
+% Create Cell array with parameter nales
 paramList = cellstr(['C1'; 'C2']);
 
-paramValues.C1 = 1000:500:2000;
-paramValues.C2 = 74:20:214;
-
-
+% Create structure with corresponding parameter values
+paramValues.C1 = 1000:500:3000;
+paramValues.C2 = 74:40:214;
 
 % Initial conditions
 liState = [15000; % Glucose
@@ -25,12 +25,15 @@ time = [0, 5000];
 % Time to start measuring baseline [G] from
 tmin = 3000;
 
+warning('off', 'MATLAB:mir_warning_maybe_uninitialized_temporary');
+
 
 for j=1:length(paramList)
     param = char(paramList(j));
-    disp(
+    fprintf('Simulating %s \n', param)
     return1 = zeros(length(paramValues.(param)), 3);
-    return2= zeros(length(paramValues.(param)), 3);
+    return2 = zeros(length(paramValues.(param)), 3);
+    baseLines = zeros(length(paramValues.(param)), 3);
     
     for i=1:length(paramValues.(param))
         % Import constants
@@ -45,58 +48,64 @@ for j=1:length(paramList)
         const.(param)= paramValues.(param)(i);
 
         % Solve equations
-        sol = liSolver(liState, const, time);
-        [t, y] = sturisSolver(sturisState, const, time);
+        solLi = liSolver(liState, const, time);
+        [tSt, ySt] = sturisSolver(sturisState, const, time);
         [tT, yT] = tolicSolver(sturisState, const, time);
 
         % Vector of baseline values for Li, Sturis, Tolic
-        baseLines = [mean(sol.y(1, sol.x>tmin)), mean(y(t>tmin, 3)),...
+        baseLines(i,:) = [mean(solLi.y(1, solLi.x>tmin)), mean(ySt(tSt>tmin, 3)),...
                     mean(yT(tT>tmin, 3))];
 
-        belowBaseIdxLi = find(sol.y(1,:)-baseLines(1)<0.01*baseLines(1));
-        belowBaseIdxSturis = find(y(:,3)-baseLines(2)<0.01*baseLines(2));
-        belowBaseIdxTolic = find(yT(:,3)-baseLines(3)<0.01*baseLines(3));
+        belowBaseIdxLi = find(solLi.y(1,:)-baseLines(i,1)<0.01*baseLines(i,1));
+        belowBaseIdxSturis = find(ySt(:,3)-baseLines(i,2)<0.01*baseLines(i,2));
+        belowBaseIdxTolic = find(yT(:,3)-baseLines(i,3)<0.01*baseLines(i,3));
 
-        [peakLi, locLi] = findpeaks(-sol.y(1,:), sol.x, 'MinPeakProminence', 2);
+        [peakLi, locLi] = findpeaks(-solLi.y(1,:), solLi.x, 'MinPeakProminence', 2);
         try
-            aboveBaseIdxLi = find(sol.y(1,:)>baseLines(1) & sol.x>locLi(1));
+            aboveBaseIdxLi = find(solLi.y(1,:)>baseLines(i,1) & solLi.x>locLi(1));
         catch 
             aboveBaseIdxLi = [1];
         end
 
-        [peakSt, locSt] = findpeaks(-y(:,3), t, 'MinPeakProminence', 2);
+        [peakSt, locSt] = findpeaks(-ySt(:,3), tSt, 'MinPeakProminence', 2);
         try
-            aboveBaseIdxSturis = find(y(:,3)>baseLines(2) & t>locSt(1));
+            aboveBaseIdxSturis = find(ySt(:,3)>baseLines(i,2) & tSt>locSt(1));
         catch 
             aboveBaseIdxSturis = [1];
         end
 
         [peakTl, locTl] = findpeaks(-yT(:,3), tT, 'MinPeakProminence', 2);
         try
-            aboveBaseIdxTolic = find(yT(:,3)>baseLines(3) & tT>locSt(1));
+            aboveBaseIdxTolic = find(yT(:,3)>baseLines(i,3) & tT>locSt(1));
         catch 
             aboveBaseIdxTolic = [1];
         end
 
-
-        return1(i, :) = [sol.x(belowBaseIdxLi(1)), t(belowBaseIdxSturis(1))...
+        return1(i, :) = [solLi.x(belowBaseIdxLi(1)), tSt(belowBaseIdxSturis(1))...
                          tT(belowBaseIdxTolic(1))];
-        return2(i, :) = [sol.x(aboveBaseIdxLi(1)), t(aboveBaseIdxSturis(1)),...
+        return2(i, :) = [solLi.x(aboveBaseIdxLi(1)), tSt(aboveBaseIdxSturis(1)),...
                          tT(aboveBaseIdxTolic(1))];
     end
 
-    return1
     %% Plotting
-    figure()
+    h = figure();
     hold on
-    subplot(2,1,1)
+    subplot(3,1,1)
     plot(paramValues.(param), return1)
-    ylabel('Return Time (min)')
+    ylabel('t_{R,1} (min)')
+    xlim([paramValues.(param)(1) paramValues.(param)(end)])
     legend('Li', 'Sturis', 'Tolic')
-    subplot(2,1,2)
+    subplot(3,1,2)
     plot(paramValues.(param), return2)
+    ylabel('t_{R,2} (min)')
+    xlim([paramValues.(param)(1) paramValues.(param)(end)])
+    subplot(3,1,3)
+    plot(paramValues.(param), baseLines./100)
+    ylabel('[G]_B (mg/ml)')
     xlabel('Paramter Value')
-    ylabel('Return Time (min)')
-    %%
+    xlim([paramValues.(param)(1) paramValues.(param)(end)])
+    savefig(h, strcat('~/scratch/', param))
+    saveas(h, strcat('~/scratch/', param, '.png'))
+
 end
 toc()
