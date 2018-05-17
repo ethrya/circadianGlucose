d80886c12d67193e0cc99a255967bbad8e3f6c8c
Ethan Ryan <erya7975@uni.sydney.edu.au>
2018-05-17 10:38:00 +1000

    Fixed error in limit cycle.

--- +utils/baselineAmplitude.m
+++ +utils/baselineAmplitude.m
@@ -5,11 +5,11 @@ function tMin = baselineAmplitude(t, G, tMax)
 % Test if steady state is a limit cycle
 gSteady = G(t>tMax);
 stableMean = mean(gSteady);
-if (max(gSteady)-min(gSteady))/stableMean>0.05
+if (max(gSteady)-min(gSteady))>0.1
     % if it is a limit cycle. Find peaks in G
     [gPeak, tPeak] = findpeaks(G, t);
     % Find mean max value of steady peaks
-    peakLimit = mean(gPeak(t>tMax));
+    peakLimit = mean(gPeak(tPeak>tMax));
     % Find first peak within 1% of this amplitude
     peaks_near_limit = tPeak(abs(gPeak-peakLimit)/peakLimit<0.01);
     tMin = peaks_near_limit(1);
@@ -17,7 +17,7 @@ else
     [~, tPeak] = findpeaks((G-stableMean)/(stableMean),...
         t, 'MinPeakHeight', 0.01);
     if isempty(tPeak)
-        tPeak = [0];
+        tPeak = 0;
     end
     reasonableG = t(abs(G-stableMean)/stableMean<0.01 & t>max(tPeak));
     tMin = reasonableG(1);
