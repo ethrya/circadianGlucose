ae2571552e8cde96e209fed2d78c571ed976e9d7
Ethan Ryan <erya7975@uni.sydney.edu.au>
2018-05-22 09:23:25 +1000

    Minor changes to parameters

--- compare_sturis_li.m
+++ compare_sturis_li.m
@@ -4,24 +4,18 @@ clear all;
 const = models.constants;
 
 %Change constants from default values
-const.tau1 = 7;
-const.tau2 = 36;
-const.td = 36;
 const.Gin = 0;
-const.C3 = 1100;
-%const.tp = 8;
-%const.alpha = 0.41;
 
-const.g = 1;
+const.g = 0.00;
 
 % Initial conditions for Li
-liState = [14000; % Glucose
+liState = [20000; % Glucose
     40]; % Insulin
 
 % Initial condition for Sturis and Tolic
 sturisState = [40; % Ip
     40; % Ii
-    14000; % G
+    20000; % G
     0; % x1
     0; % x2
     0]; % x3
@@ -52,14 +46,14 @@ plot(tSt/60,Ip)
 plot(tT/60, yT(:,1)/const.Vp)
 hold off
 ylabel('Insulin (\muU/ml)')
-legend('Li et al. (2006)', 'Sturis et al. (1991)', 'Tolic et al. (2000)')
+%legend('Li et al. (2006)', 'Sturis et al. (1991)', 'Tolic et al. (2000)')
 % Plot [I]
 subplot(2,1,2)
 hold on
 plot(solLi.x/60, solLi.y(1,:)/(10*const.Vg));
 plot(tSt/60,G)
 plot(tT/60, yT(:,3)/(10*const.Vg))
-plot([0 max(tT)/60], [mean(solLi.y(1,solLi.x>600)) mean(solLi.y(1,solLi.x>600))]/(10*const.Vg))
+%plot([0 max(tT)/60], [mean(solLi.y(1,solLi.x>600)) mean(solLi.y(1,solLi.x>600))]/(10*const.Vg))
 hold off
 xlabel('Time (h)')
 ylabel('Glucose (mg/dl)')
