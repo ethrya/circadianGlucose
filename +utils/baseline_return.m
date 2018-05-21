3430ac82db0cce147bdd8feb6cdff2c874d7694d
Ethan Ryan <erya7975@uni.sydney.edu.au>
2018-05-22 08:42:14 +1000

    Added linear interpolation and reduced tolerance

--- +utils/baseline_return.m
+++ +utils/baseline_return.m
@@ -1,11 +1,15 @@
 function returnTime = baseline_return(tIn, G, tMin)
     %% Time to return to baseline concentration
+    % Interpolate times and G values
+    tIp = min(tIn):0.001:max(tIn);
+    GIp = interp1(tIn, G, tIp);
+    
     % Takes a vector of times and glucose concentrations
-    baselineAmount = mean(G(tIn>tMin));
+    baselineAmount = mean(GIp(tIp>tMin));
     
     % Calculate index of first instance when within 1% of baseline
-    belowBaseIdx = find(G-baselineAmount<0.01*abs(baselineAmount));
+    belowBaseIdx = find(GIp-baselineAmount<0.001*abs(baselineAmount));
     
     % Time of return to baseline
-    returnTime = tIn(belowBaseIdx(1));
+    returnTime = tIp(belowBaseIdx(1));
 end
