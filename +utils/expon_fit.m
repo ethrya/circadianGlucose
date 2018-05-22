a34f6c1f47176ef2abba764c8f24e70f1e5c5316
Ethan Ryan <erya7975@uni.sydney.edu.au>
2018-05-22 17:47:32 +1000

    fixed bug for incorrect if statement order

--- +utils/expon_fit.m
+++ +utils/expon_fit.m
@@ -5,17 +5,17 @@ G = interp1(t, G, tIp);
 
 [gPeak, tPeak] = findpeaks(G, tIp);
 
-if isempty(tPeak)
+if length(tPeak)>0
     try
         gBase = mean(gPeak(tPeak>tMax));
     catch
         gBase = mean(G(tIp>tMax));
         warning("No peaks after tMax")
     end
-    gPeak = gPeak-gbase;
+    gPeak = gPeak-gBase;
     f = fit(tPeak', gPeak', 'exp1');
 else
     gBase = mean(G(tIp>tMax));
     G = G - gBase;
-    f = fit(tIp, G, 'exp1');
+    f = fit(tIp', G', 'exp1');
 end
