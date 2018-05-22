98f7c5719bda02f55d3f92f4bea93c91e29205ee
Ethan Ryan <erya7975@uni.sydney.edu.au>
2018-05-22 14:27:37 +1000

    Changed definitial to first time after last significant peak

--- +utils/baselineAmplitude.m
+++ +utils/baselineAmplitude.m
@@ -1,7 +1,9 @@
 function tMin = baselineAmplitude(t, G, tMax)
 %BASELINEAMPLITUDE calculate the time for the functions amplitude to return
 %to its basemine
-
+tIp = min(t):0.001:max(t);
+G = interp1(t, G, tIp);
+t = tIp;
 % Test if steady state is a limit cycle
 gSteady = G(t>tMax);
 stableMean = mean(gSteady);
@@ -11,16 +13,17 @@ if (max(gSteady)-min(gSteady))>0.1
     % Find mean max value of steady peaks
     peakLimit = mean(gPeak(tPeak>tMax));
     % Find first peak within 1% of this amplitude
-    peaks_near_limit = tPeak(abs(gPeak-peakLimit)/peakLimit<0.01);
+    peaks_near_limit = tPeak(abs(gPeak-peakLimit)/peakLimit<0.001);
     tMin = peaks_near_limit(1);
 else
-    [gPeak, tPeak] = findpeaks((G-stableMean), t);
-    % Test if critically or uncritically damped
+    % Find peaks with amplitude>0.005
+    [gPeak, tPeak] = findpeaks((G-stableMean), t, 'MinPeakHeight', 0.005*stableMean);
+    % Test if critically or uncritically damped (i.e are there peaks?)
     if isempty(tPeak)
         % For critically damped, just use time within
-        reasonableG = t(abs(G-stableMean)<0.01);
+        reasonableG = t(abs(G-stableMean)<0.001);
     else
-        reasonableG = tPeak(gPeak<0.01);        
+        reasonableG = t(abs(G-stableMean)<0.001 & t>tPeak(end));        
     end
     tMin = reasonableG(1);
 end
