6dffaaf466297a6a0df259fde94c9aae77197e1e
Ethan Ryan <ethanryan9@gmail.com>
2018-05-22 17:35:14 +1000

    Added additional cases to exponfit

--- +utils/expon_fit.m
+++ +utils/expon_fit.m
@@ -5,9 +5,17 @@ G = interp1(t, G, tIp);
 
 [gPeak, tPeak] = findpeaks(G, tIp);
 
-gPeak = gPeak-mean(gPeak(tPeak>tMax));
-
-f = fit(tPeak', gPeak', 'exp1');
-
-plot(f, tPeak, gPeak)
+if isempty(tPeak)
+    try
+        gBase = mean(gPeak(tPeak>tMax));
+    catch
+        gBase = mean(G(tIp>tMax));
+        warning("No peaks after tMax")
+    end
+    gPeak = gPeak-gbase;
+    f = fit(tPeak', gPeak', 'exp1');
+else
+    gBase = mean(G(tIp>tMax));
+    G = G - gBase;
+    f = fit(tIp, G, 'exp1');
 end
