5066250b6db31184429c6c09af8e86679087042a
Ethan Ryan <ethanryan9@gmail.com>
2018-05-23 19:47:01 +1000

    Added contstant to exponental fitter

--- +utils/expon_fit.m
+++ +utils/expon_fit.m
@@ -3,6 +3,8 @@ function f = expon_fit(t, G, tMax)
 tIp = min(t):0.001:max(t);
 G = interp1(t, G, tIp);
 
+exponConst = fittype('a*exp(b*x)+c');
+
 [gPeak, tPeak] = findpeaks(G, tIp);
 
 if length(tPeak)>0
@@ -12,10 +14,10 @@ if length(tPeak)>0
         gBase = mean(G(tIp>tMax));
         warning("No peaks after tMax")
     end
-    gPeak = gPeak-gBase;
-    f = fit(tPeak', gPeak', 'exp1', 'StartPoint', [20000, -0.01]);
+    %gPeak = gPeak-gBase;
+    f = fit(tPeak', gPeak', exponConst, 'StartPoint', [G(1), -0.01, gBase]);
 else
     gBase = mean(G(tIp>tMax));
-    G = G - gBase;
-    f = fit(tIp', G', 'exp1', 'StartPoint', [20000, -0.01]);
+    %G = G - gBase;
+    f = fit(tIp', G', exponConst, 'StartPoint', [G(1), -0.01, gBase]);
 end
