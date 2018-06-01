35245d2035cca5242a40a1bcac2772ac092f4911
Ethan Ryan <erya7975@uni.sydney.edu.au>
2018-06-01 15:45:16 +1000

    Added line histograms and overlay

--- +parameter_sensitivity/EEs_plots.m
+++ +parameter_sensitivity/EEs_plots.m
@@ -31,144 +31,118 @@ parameter_sensitivity.plot_EET(paramList, muStarT1, sigmaT1)
 saveas(gcf,strcat('/suphys/erya7975/Dropbox (Sydney Uni Student)/Circadian Glucose Dynamics/Sim_results/2018-05-30/EET_run_',...
     num2str(runNo.','%02d'), 't1_bar.png'))
 %% Histograms
-model = 1;
 figure()
+modelColors = [1 0 0; 0 1 0; 0 0 1];
 for i=2:k+1
     subplot(4,5,i-1)
-    hist(tau(:,i,model))
-    title(paramList(i-1))
-end
-saveas(gcf,strcat('/suphys/erya7975/Dropbox (Sydney Uni Student)/Circadian Glucose Dynamics/Sim_results/2018-05-30/EET_run_',...
-    num2str(runNo.','%02d'), 'tau_li_hist.png'))
-model = 2;
-figure()
-for i=2:k+1
-    subplot(4,5,i-1)
-    hist(tau(:,i,model))
-    title(paramList(i-1))
-end
-saveas(gcf,strcat('/suphys/erya7975/Dropbox (Sydney Uni Student)/Circadian Glucose Dynamics/Sim_results/2018-05-30/EET_run_',...
-    num2str(runNo.','%02d'), 'tau_sturis_hist.png'))
-figure()
-model=3;
-for i=2:k+1
-    subplot(4,5,i-1)
-    hist(tau(:,i,model))
-    title(paramList(i-1))
-end
-saveas(gcf,strcat('/suphys/erya7975/Dropbox (Sydney Uni Student)/Circadian Glucose Dynamics/Sim_results/2018-05-30/EET_run_',...
-    num2str(runNo.','%02d'), 'tau_tolic_hist.png'))
-%% Histograms
-model = 1;
-figure()
-for i=2:k+1
-    subplot(4,5,i-1)
-    hist(Gb(:,i,model))
-    title(paramList(i-1))
-end
-saveas(gcf,strcat('/suphys/erya7975/Dropbox (Sydney Uni Student)/Circadian Glucose Dynamics/Sim_results/2018-05-30/EET_run_',...
-    num2str(runNo.','%02d'), 'gb_li_hist.png'))
-model = 2;
-figure()
-for i=2:k+1
-    subplot(4,5,i-1)
-    hist(Gb(:,i,model))
-    title(paramList(i-1))
-end
-saveas(gcf,strcat('/suphys/erya7975/Dropbox (Sydney Uni Student)/Circadian Glucose Dynamics/Sim_results/2018-05-30/EET_run_',...
-    num2str(runNo.','%02d'), 'gb_sturis_hist.png'))
-figure()
-model=3;
-for i=2:k+1
-    subplot(4,5,i-1)
-    hist(Gb(:,i,model))
-    title(paramList(i-1))
-end
-saveas(gcf,strcat('/suphys/erya7975/Dropbox (Sydney Uni Student)/Circadian Glucose Dynamics/Sim_results/2018-05-30/EET_run_',...
-    num2str(runNo.','%02d'), 'gb_tolic_hist.png'))
-%% Convergence plot
-models = cellstr(['Li    '; 'Sturis'; 'Tolic ']);
-muStar = zeros(k, 3, r);mu = zeros(k, 3, r); sigma = zeros(k, 3, r);
-for i=1:r
-    [muStar(:,:,i), mu(:,:,i), sigma(:,:,i)] = parameter_sensitivity.EET_means(EEs(1:i,:,:), k);
-end
-figure()
-for j = 1:3
-    subplot(2,3,j)
     hold on
-    for i = 1:k
-        plot(1:r, reshape(muStar(i,j,:),[r 1]))
+    for model = 1:3
+        h = histogram(tau(:,i,model), -300:20:0);
+        h.FaceColor = 'none';
+        h.EdgeColor = modelColors(model,:);
+        title(paramList(i-1))
+        xlabel('\tau (min)')
+        xlim([min(min(min(tau))) max(max(max(tau)))])
     end
-    ylabel('\mu^*')
-    title(models(j))
+    hold off
 end
-
-for j = 1:3
-    subplot(2,3,j+3)
-    hold on
-    for i = 1:k
-        plot(1:r, reshape(sigma(i,j,:),[r 1]))
-    end
-    ylabel('\sigma')
-    xlabel('r')
-end
-saveas(gcf,strcat('/suphys/erya7975/Dropbox (Sydney Uni Student)/Circadian Glucose Dynamics/Sim_results/2018-05-30/EET_run_',...
-    num2str(runNo.','%02d'), 'Gb_EET_convergence.png'))
 
 %%
-%% Convergence plot
-muStar = zeros(k, 3, r);mu = zeros(k, 3, r); sigma = zeros(k, 3, r);
-for i=1:r
-    [muStar(:,:,i), mu(:,:,i), sigma(:,:,i)] = parameter_sensitivity.EET_means(EEsReturnAmplitude(1:i,:,:), k);
-end
 figure()
-for j = 1:3
-    subplot(2,3,j)
+modelColors = [1 0 0; 0 1 0; 0 0 1];
+for i=2:k+1
+    subplot(4,5,i-1)
     hold on
-    for i = 1:k
-        plot(1:r, reshape(muStar(i,j,:),[r 1]))
+    for model = 1:3
+        h = histogram(Gb(:,i,model), 0:1000:20000);
+        h.FaceColor = 'none';
+        h.EdgeColor = modelColors(model,:);
+        title(paramList(i-1))
+        xlabel('G_b (mg)')
+        xlim([min(min(min(Gb))) max(max(max(Gb)))])
     end
-    ylabel('\mu^*')
-    title(models(j))
+    hold off
 end
-
-for j = 1:3
-    subplot(2,3,j+3)
-    hold on
-    for i = 1:k
-        plot(1:r, reshape(sigma(i,j,:),[r 1]))
-    end
-    ylabel('\sigma')
-    xlabel('r')
-end
-saveas(gcf,strcat('/suphys/erya7975/Dropbox (Sydney Uni Student)/Circadian Glucose Dynamics/Sim_results/2018-05-30/EET_run_',...
-    num2str(runNo.','%02d'), 'tau_EET_convergence.png'))
 
 %%
-%% Convergence plot
-muStar = zeros(k, 3, r);mu = zeros(k, 3, r); sigma = zeros(k, 3, r);
-for i=1:r
-    [muStar(:,:,i), mu(:,:,i), sigma(:,:,i)] = parameter_sensitivity.EET_means(EEsReturnTime(1:i,:,:), k);
-end
-figure()
-for j = 1:3
-    subplot(2,3,j)
-    hold on
-    for i = 1:k
-        plot(1:r, reshape(muStar(i,j,:),[r 1]))
-    end
-    ylabel('\mu^*')
-    title(models(j))
-end
 
-for j = 1:3
-    subplot(2,3,j+3)
-    hold on
-    for i = 1:k
-        plot(1:r, reshape(sigma(i,j,:),[r 1]))
-    end
-    ylabel('\sigma')
-    xlabel('r')
+
+%% Overall Histogram G_b
+hold on
+modelColors = [0 0 1; 0 0 0;  0.5 0 0.5];
+
+for model = 1:3
+    h = histogram(Gb(:,:,model), 0:1000:20000);
+    h.FaceColor = 'none';
+    h.LineWidth = 2;
+    h.DisplayStyle = 'stairs';
+    h.EdgeColor = modelColors(model,:);
 end
-saveas(gcf,strcat('/suphys/erya7975/Dropbox (Sydney Uni Student)/Circadian Glucose Dynamics/Sim_results/2018-05-30/EET_run_',...
-    num2str(runNo.','%02d'), 't1_EET_convergence.png'))
+xlabel('G_b (mg)')
+xlim([min(min(min(Gb))) max(max(max(Gb)))])
+line([7000, 7000], ylim, 'LineWidth',2, 'Color', 'g')
+line([10000, 10000], ylim, 'LineWidth',2, 'Color', [1 0.5 0])
+line([12500, 12500], ylim, 'LineWidth',2, 'Color', 'r')
+legend('Li', 'Sturis', 'Tolic', 'Normal', 'High Risk', 'Diabetes')
+ylabel('Number of simulations')
+hold off
+
+
+
+% %%
+% %% Convergence plot
+% muStar = zeros(k, 3, r);mu = zeros(k, 3, r); sigma = zeros(k, 3, r);
+% for i=1:r
+%     [muStar(:,:,i), mu(:,:,i), sigma(:,:,i)] = parameter_sensitivity.EET_means(EEsReturnAmplitude(1:i,:,:), k);
+% end
+% figure()
+% for j = 1:3
+%     subplot(2,3,j)
+%     hold on
+%     for i = 1:k
+%         plot(1:r, reshape(muStar(i,j,:),[r 1]))
+%     end
+%     ylabel('\mu^*')
+%     title(models(j))
+% end
+% 
+% for j = 1:3
+%     subplot(2,3,j+3)
+%     hold on
+%     for i = 1:k
+%         plot(1:r, reshape(sigma(i,j,:),[r 1]))
+%     end
+%     ylabel('\sigma')
+%     xlabel('r')
+% end
+% saveas(gcf,strcat('/suphys/erya7975/Dropbox (Sydney Uni Student)/Circadian Glucose Dynamics/Sim_results/2018-05-30/EET_run_',...
+%     num2str(runNo.','%02d'), 'tau_EET_convergence.png'))
+% 
+% %%
+% %% Convergence plot
+% muStar = zeros(k, 3, r);mu = zeros(k, 3, r); sigma = zeros(k, 3, r);
+% for i=1:r
+%     [muStar(:,:,i), mu(:,:,i), sigma(:,:,i)] = parameter_sensitivity.EET_means(EEsReturnTime(1:i,:,:), k);
+% end
+% figure()
+% for j = 1:3
+%     subplot(2,3,j)
+%     hold on
+%     for i = 1:k
+%         plot(1:r, reshape(muStar(i,j,:),[r 1]))
+%     end
+%     ylabel('\mu^*')
+%     title(models(j))
+% end
+% 
+% for j = 1:3
+%     subplot(2,3,j+3)
+%     hold on
+%     for i = 1:k
+%         plot(1:r, reshape(sigma(i,j,:),[r 1]))
+%     end
+%     ylabel('\sigma')
+%     xlabel('r')
+% end
+% saveas(gcf,strcat('/suphys/erya7975/Dropbox (Sydney Uni Student)/Circadian Glucose Dynamics/Sim_results/2018-05-30/EET_run_',...
+%     num2str(runNo.','%02d'), 't1_EET_convergence.png'))
