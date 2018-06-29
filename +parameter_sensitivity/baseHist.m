d4b73e0e977ccf1b392188c87055a05ac41667bc
Ethan Ryan <ethanryan9@gmail.com>
2018-06-29 13:11:39 +1000

    Updated hist so only G, IR and beta shown.

--- +parameter_sensitivity/baseHist.m
+++ +parameter_sensitivity/baseHist.m
@@ -2,15 +2,15 @@ function baseHist(baseG, baseI)
 
 baseG = baseG/100;
 baseI = baseI/10;
-subplot(2,2,1)
+subplot(3,1,1)
 hold on
-modelColors = [0 0 1; 0 0 0;  0.5 0 0.5];
+modelColors = [0.74 .45 0; 0 0 0;  0 0.45 0.74];
 minG = min(min(min(baseG))); maxG = max(max(max(baseG)));
 maxN = 120;
-rectangle('Position',[minG, 0, 70-minG, maxN+10], 'FaceColor', [1 0 0 0.25])
-rectangle('Position',[70,0,30,maxN+10], 'FaceColor', [0 1 0 0.25])
-rectangle('Position',[100,0,25,maxN+10], 'FaceColor', [1 1 0 0.25])
-rectangle('Position',[125,0,maxG,maxN+10], 'FaceColor', [1 0 0 0.25])
+rectangle('Position',[minG, 0, 70-minG, maxN+10], 'FaceColor', [1 .5 0 0.15],'LineStyle','none')
+rectangle('Position',[70,0,30,maxN+10], 'FaceColor', [0 1 0 0.15],'LineStyle','none')
+rectangle('Position',[100,0,25,maxN+10], 'FaceColor', [1 1 0 0.15],'LineStyle','none')
+rectangle('Position',[125,0,maxG,maxN+10], 'FaceColor', [1 0 0 0.15],'LineStyle','none')
 
 for model = 1:3
     h = histogram(real(baseG(:,model)), 0:5:200);
@@ -21,50 +21,56 @@ for model = 1:3
 end
 xlabel('[G]_b (mg/dl)')
 xlim([min(min(min(baseG))) max(max(max(baseG)))])
+ylim([0 120])
 % line([70, 70], ylim, 'LineWidth',2, 'Color', 'g')
 % line([100, 100], ylim, 'LineWidth',2, 'Color', [1 0.5 0])
 % line([125, 125], ylim, 'LineWidth',2, 'Color', 'r')
 
-ylabel('Number of simulations')
-hold off
-
-subplot(2,2,2)
-
-hold on
-for model = 1:3
-    h = histogram(real(baseI(:,model)), 0:.5:200);
-    %h = histogram(360*real(baseI(:,model))./(real(baseG(:,model))-63), 0:5:200);
-    h.LineWidth = 2;
-    h.DisplayStyle = 'stairs';
-    h.EdgeColor = modelColors(model,:);
-end
-ylabel('Number of simulations')
-xlim([min(min(min(baseI))) max(max(max(baseI)))])
-%xlabel('\beta cell functionality (%)')
-xlabel('[I]_B (\mu U/ml)')
-LH(1) = plot(nan,nan,'color',modelColors(1,:)); L{1} = 'Li';
-LH(2) = plot(nan,nan,'color',modelColors(2,:)); L{2} = 'Sturis';
-LH(3) = plot(nan,nan,'color',modelColors(3,:)); L{3} = 'Tolic';
+LH(1) = plot(nan,nan,'color',modelColors(1,:),'LineWidth',2); L{1} = 'Li';
+LH(2) = plot(nan,nan,'color',modelColors(2,:),'LineWidth',2); L{2} = 'Sturis';
+LH(3) = plot(nan,nan,'color',modelColors(3,:),'LineWidth',2); L{3} = 'Tolic';
 legend(LH, L)%, 'Normal', 'High Risk', 'Diabetes')
+
+ylabel('n')
 hold off
 
-subplot(2,2,3)
+%subplot(2,2,2)
+
+% hold on
+% for model = 1:3
+%     h = histogram(real(baseI(:,model)), 0:.5:200);
+%     %h = histogram(360*real(baseI(:,model))./(real(baseG(:,model))-63), 0:5:200);
+%     h.LineWidth = 2;
+%     h.DisplayStyle = 'stairs';
+%     h.EdgeColor = modelColors(model,:);
+% end
+% ylabel('Number of simulations')
+% xlim([min(min(min(baseI))) max(max(max(baseI)))])
+% %xlabel('\beta cell functionality (%)')
+% xlabel('[I]_B (\mu U/ml)')
+% LH(1) = plot(nan,nan,'color',modelColors(1,:)); L{1} = 'Li';
+% LH(2) = plot(nan,nan,'color',modelColors(2,:)); L{2} = 'Sturis';
+% LH(3) = plot(nan,nan,'color',modelColors(3,:)); L{3} = 'Tolic';
+% legend(LH, L)%, 'Normal', 'High Risk', 'Diabetes')
+% hold off
+
+subplot(3,1,2)
 hold on
 for model = 1:3
-    h = histogram(real(baseI(:,model)).*real(baseG(:,model))/402);%, 0:.5:200);
+    h = histogram(real(baseI(:,model)).*real(baseG(:,model))/402, 0:.1:5);
     %h = histogram(360*real(baseI(:,model))./(real(baseG(:,model))-63), 0:5:200);
     h.LineWidth = 2;
     h.DisplayStyle = 'stairs';
     h.EdgeColor = modelColors(model,:);
 end
-ylabel('Number of simulations')
-%xlim([min(min(min(baseI))) max(max(max(baseI)))])
+ylabel('n')
+xlim([0 5])
 xlabel('HOMA-IR')
 %xlabel('[I]_B (\mu U/ml)')
 hold off
 
 
-subplot(2,2,4)
+subplot(3,1,3)
 
 hold on
 for model = 1:3
@@ -74,8 +80,8 @@ for model = 1:3
     h.DisplayStyle = 'stairs';
     h.EdgeColor = modelColors(model,:);
 end
-ylabel('Number of simulations')
-%xlim([min(min(min(baseI))) max(max(max(baseI)))])
+ylabel('n')
+xlim([0 200])
 xlabel('\beta cell functionality (%)')
 %xlabel('[I]_B (\mu U/ml)')
 hold off
