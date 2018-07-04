6c803becd714efb26ffbf81ae8655200dce4d314
Ethan Ryan <ethanryan9@gmail.com>
2018-07-04 17:56:41 +1000

    Minor bug fixes

--- +utils/rk4Fixed.m
+++ +utils/rk4Fixed.m
@@ -1,4 +1,4 @@
-function y = rk4Fixed(model, state, const, times);
+function y = rk4Fixed(model, state, const, times)
 % Implementation of a user defined rk4 ODE solver
 
 %% Create empty vector of y solutions and add IC
@@ -7,6 +7,6 @@ y(1, :) = state;
 
 %% Loop over implementation
 for i=1:length(times)-1
-    y(i+1,:) = utils.rk4step(model, t0, t1, yIn, const);
+    y(i+1,:) = utils.rk4step(model, times(i), times(i+1), y(i,:), const);
 end
 end
