e6a374869324982adcd6e112b3d3402178fc326e
Ethan Ryan <erya7975@uni.sydney.edu.au>
2018-07-24 13:49:24 +1000

    Bug fixes

--- +protocols/tolerance_rc.m
+++ +protocols/tolerance_rc.m
@@ -1,9 +1,10 @@
 % Script to create a tolerance response curve for meals at different times
 % of the day
-function [gPeak, mealTimes] = toleranceRC(mealSpace)
+function [gPeak, mealTimes] = tolerance_rc(mealSpace)
 
 const = models.constants;
 const.g2 = 0.3;
+const.phi0 = 3*pi/4;
 
 
 % Set a default mealSpace of 4 hours
@@ -29,20 +30,31 @@ time = [0, 1440*2.5];
 deltaT = 1; % Time step (min)
 tInt = time(1):deltaT:time(end);
 
+% Vector of initial meal times
 mealTimes = 0:mealSpace:24;
-
 gPeak = zeros(1,length(mealTimes));
 
+% Calculate response to meals
 for mealNo = 1:length(mealTimes)
+    % Vector of times corresp to meal
     const.times = 0:deltaT/2:2.5*1440;
-    mealIdx = mealTimes(mealNo)*deltaT/2+24*deltaT/2;
+    % Index of meal
+    mealIdx = (mealTimes(mealNo)+24)*2*60/deltaT;
+    
+    % Gin for fasting except 500 idx after a meal occuring
     const.Gin = zeros(1, length(const.times));
     const.Gin(mealIdx:mealIdx+500) = protocols.chenMeal(0:500, 80, 3400);
     
+    % Solve ODEs for meal
     ySt = utils.rk4Fixed(@models.sturisCirc, sturisState, const, tInt);
-
-    gPeak(mealNo) = max(ySt(tInt>1440,3));
     
+    % Calculate peak glucose within 250 idx of meal
+    gPeak(mealNo) = max(ySt(tInt>mealIdx/2 & tInt<mealIdx/2+250,3));
+
 end
 
+%% Plot peak glucose vs meal time
 plot(mealTimes, gPeak)
+xlabel('Meal Time (zt)')
+ylabel('Peak Glucose')
+end
