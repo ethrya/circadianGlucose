c0b7cc993b2a1f4dce249602818cfb7234c68f5c
ethrya <ethanryan9@gmail.com>
2018-07-26 10:07:19 +1000

    Added ISR plot

--- circTests.m
+++ circTests.m
@@ -2,12 +2,15 @@
 % Run a test of the current model with baseline glucose and create a meal
 % response curve
 %% prelimaries
+clear
 tic
 const = models.constants;
-const.g1 = 0.1;
-const.phi1 = -3*pi/4;
+const.g1 = 0.2;
+const.phi1 = 0;
 const.g2 = 0;
-const.phi2 = -3*pi/4;
+const.phi2 = 0;
+const.g3 = 0.2;
+const.phi3 = -pi/2;
 
 % Initial condition for Sturis and Tolic
 sturisState = [40; % Ip
@@ -21,13 +24,18 @@ sturisState = [40; % Ip
 time = [0, 1440*2.5];
 
 %% Baseline Simulations
-const.Gin = 0;
+const.Gin = 50;
 
 tSt = 0:1440*2.5; tStC = 0:1440*2.5;
 
 ySt = utils.rk4Fixed(@models.sturis, sturisState, const, tSt);
 
 yStC = utils.rk4Fixed(@models.sturisCirc, sturisState, const, tSt);
+
+%% Calculate ISR
+ISR = models.funcs.f1(ySt(:,3),const);
+const.C = 2*pi*tStC'/1440;
+ISR_circ = models.funcs.f1(yStC(:,3),const);
 
 % Plots
 %% Plot [G] and [I] (in % of baseline units) vs t for all 3 models.
@@ -39,7 +47,7 @@ GC = yStC(:,3)/(const.Vg*10); %[G]=G/Vg mg/dl
 
 figure()
 % Plot [I]
-subplot(3,1,1)
+subplot(4,1,1)
 hold on
 plot(tSt/60, utils.meanPercent(Ip, 1440))
 plot(tStC/60,utils.meanPercent(IpC, 1440),'k')
@@ -51,19 +59,29 @@ xticklabels([0:4:24])
 xlim([24 48])
 
 % Plot [G]
-subplot(3,1,2)
+subplot(4,1,2)
 hold on
 plot(tSt/60, utils.meanPercent(G, 1440))
 plot(tStC/60, utils.meanPercent(GC, 1440),'k')
 hold off
-xlabel('Time (hours)')
 ylabel('[G] (% of mean)')
 xticks([24:4:48])
 xticklabels([0:4:24])
 xlim([24 48])
 
+% Plot ISR f1
+subplot(4,1,3)
+hold on
+plot(tSt/60, utils.meanPercent(ISR, 1440))
+plot(tStC/60, utils.meanPercent(ISR_circ, 1440),'k')
+hold off
+xlabel('Time (hours)')
+ylabel('ISR (% of mean)')
+xticks([24:4:48])
+xticklabels([0:4:24])
+xlim([24 48])
 %% Tolerance
-subplot(3,1,3)
-protocols.tolerance_rc(const, 2);
+subplot(4,1,4)
+protocols.tolerance_rc(const, 4);
 
 toc
