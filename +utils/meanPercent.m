b6b01dc1d6f4a8f92bcdaa2f96dac3761d229de5
ethrya <ethanryan9@gmail.com>
2018-07-27 11:51:23 +1000

    Set default tMax=end

--- +utils/meanPercent.m
+++ +utils/meanPercent.m
@@ -4,12 +4,16 @@ function relValue = meanPercent(inputVariable, tMin, tMax)
 % INPUT VARIABLES:
 % inputVariable - 1D vector (e.g. Insulin and glucose concentration)
 % tMin - time index to start calculating baseline from
-% tMax - time index to end calculating baseline
+% tMax (default=end) - time index to end calculating baseline
 % OUTPUT VARIABLES:
 % relValue - transformed inputVariable
 
 
-%%
+% set tMax to the end of inputVariable by default
+if nargin == 2
+    tMax = length(inputVariable);
+end
+
 % Calculate baseline
 baseline = mean(inputVariable(tMin:tMax));
 
