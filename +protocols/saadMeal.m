aca6dd09292b4892da391db3cdc944fa8958449b
ethrya <ethanryan9@gmail.com>
2018-07-31 15:55:13 +1000

    Bug fix dot notation

--- +protocols/saadMeal.m
+++ +protocols/saadMeal.m
@@ -1,5 +1,5 @@
 function mealG = saadMeal(x)
 % Function to create meal using data from Saad 2012 Figure 2A (breakfast)
 k = 2448; b = 2.099; n = 0.5849; x0 = 8.346;
-mealG = k*(x-x0)^n/b^2*exp(-(x-x0)^n/(b^2));
+mealG = k*(x-x0).^n/b^2.*exp(-(x-x0).^n/(b^2));
 end
