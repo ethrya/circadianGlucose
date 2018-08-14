a9045c10566de579356358277a8cdca618d78505
ethrya <ethanryan9@gmail.com>
2018-08-14 10:46:39 +1000

    Added circadian solver and model for tolic

---
+++ +models/tolicCirc.m
@@ -0,0 +1,47 @@
+function out = sturisCirc(t, in, const)
+% Model based on Tolic et al. (2001) with circadian effects.
+% t - time of calculation
+% in - vector of input state [Ip ,Ii, G, x1, x2, x3] at t
+% const - constants object
+
+% Constant Parameters
+Vp=const.Vp; Vi=const.Vi; E=const.E;
+tp=const.tp; ti=const.ti; td=const.td;
+
+% see if Gin is a constant and either use the constant value or linear
+% interpolation of the vector of values.
+if length(const.Gin)==1
+    Gin = const.Gin;
+elseif length(const.Gin)>1
+    try
+        Gin = const.Gin(const.times==t);
+    catch
+        Gin = interp1(const.times, const.Gin, t);
+    end
+end
+
+
+% Insulin and Glucose amounts
+Ip = in(1); Ii = in(2); G = in(3);
+
+% Convert t into circadian frequency
+const.C = 2*pi*t/1440;
+
+%% Model Equations
+out = zeros(6,1);
+out(1) = models.funcs.f1(G,const) -E*(Ip/Vp-Ii/Vi)-Ip/(tp);
+out(2) = E*(Ip/Vp-Ii/Vi)-Ii/ti;
+
+% Either calculate the value or use a glucose clamp
+if isnan(const.clamp)==1
+    out(3) = Gin-models.funcs.f2(G,const)-models.funcs.f3(G,const)*...
+        models.funcs.f4(Ii,const)+models.funcs.f5(in(6),const,'Tolic')*...
+        models.funcs.f6(G,const)-models.funcs.f7(G,const);
+else
+    out(3) = 0;
+end
+
+out(4) = 3/td*(Ip-in(4));
+out(5) = 3/td*(in(4)-in(5));
+out(6) = 3/td*(in(5)-in(6));
+end
---
+++ +sims/circSolverTolic.m
@@ -0,0 +1,141 @@
+clear;
+%% Preliminaries
+nDays = 2;
+deltaT = 0.5;
+% Import constants class
+const = models.constants;
+
+%Change constants from default values
+%[const.times, const.Gin] = protocols.IdenticalMeals(50, nDays, deltaT);
+const.times = 0:deltaT:2*1440;
+const.Gin = protocols.saad12(deltaT);
+%%
+%const.Gin = 0;
+
+const.g1 = 0.05;
+const.phi1 = 0;
+const.g2 = 0.05;
+const.phi2 = 0;
+const.g3 = 0;
+const.phi3 = 0;
+%const.td = 5;
+
+% Initial condition for Sturis and Tolic
+sturisState = [40; % Ip
+    40; % Ii
+    10000; % G
+    0; % x1
+    0; % x2 
+    0]; % x3
+
+% Integration time (min)
+time = [0, 1440*nDays];
+  
+
+%% Solve equations
+tSt = 0:1440*nDays; tStC = 0:1440*nDays;
+
+ySt = utils.rk4Fixed(@models.tolic, sturisState, const, tSt);
+%const.C1 = 1720; const.Rm = 150; const.a1 = 350;
+%const.C5 = 18.6; const.Rg = 181.5; const.alpha = 0.1168;
+
+yStC = utils.rk4Fixed(@models.tolicCirc, sturisState, const, tSt);
+
+
+%% Plotting
+% Convert glucose amounts into concentrations
+Ip = ySt(:,1)/const.Vp; %[I]=I/Vp microU/ml
+G = ySt(:,3)/(const.Vg*10); %[G]=G/Vg mg/dl
+IpC = yStC(:,1)/const.Vp; %[I]=I/Vp microU/ml
+GC = yStC(:,3)/(const.Vg*10); %[G]=G/Vg mg/dl
+
+% Plot [G] and [I] vs t for all 3 models.
+figure()
+% Plot [I]
+subplot(3,1,1)
+hold on
+plot(tSt/60, Ip)
+plot(tStC/60, IpC)
+hold off
+ylabel('[I] (\muU/ml)')
+legend('Original', 'New')
+% Plot [G]
+subplot(3,1,2)
+hold on
+plot(tSt/60, G)
+plot(tStC/60, GC)
+xticks(0:6:48)
+xticklabels(0:0.25:2)
+%plot([0 max(tT)/60], [mean(solLi.y(1,solLi.x>600)) mean(solLi.y(1,solLi.x>600))]/(10*const.Vg))
+hold off
+xlabel('Time (days)')
+ylabel('[G] (mg/dl)')
+
+subplot(3,1,3)
+hold on
+try
+    plot(const.times/1440,const.Gin)
+catch
+    plot(time/1440, [const.Gin const.Gin])
+end
+%plot([0 max(tT)/60], [mean(solLi.y(1,solLi.x>600)) mean(solLi.y(1,solLi.x>600))]/(10*const.Vg))
+hold off
+xlabel('Time (days)')
+ylabel('G_{in} (mg/min)')
+
+%% Plot [G] and [I] (in % of baseline units) vs t for all 3 models.
+figure()
+% Plot [I]
+subplot(3,1,1)
+hold on
+plot(tSt/60, utils.meanPercent(Ip, 1440))
+plot(tStC/60,utils.meanPercent(IpC, 1440))
+hold off
+ylabel('[I] (% of mean)')
+legend('Original', 'New')
+xticks(0:6:48)
+xticklabels(0:0.25:2)
+% Plot [G]
+subplot(3,1,2)
+hold on
+plot(tSt/60, utils.meanPercent(G, 1440))
+plot(tStC/60, utils.meanPercent(GC, 1440))
+%plot([0 max(tT)/60], [mean(solLi.y(1,solLi.x>600)) mean(solLi.y(1,solLi.x>600))]/(10*const.Vg))
+hold off
+xlabel('Time (days)')
+ylabel('[G] (% of mean)')
+xticks(0:6:48)
+xticklabels(0:0.25:2)
+
+subplot(3,1,3)
+hold on
+try
+    plot(const.times/1440,const.Gin)
+catch
+    plot(time/1440, [const.Gin const.Gin])
+end
+
+%plot([0 max(tT)/60], [mean(solLi.y(1,solLi.x>600)) mean(solLi.y(1,solLi.x>600))]/(10*const.Vg))
+hold off
+
+xlabel('Time (days)')
+ylabel('G_{in} (mg/min)')
+
+% %% Phase plane
+% % Plot phase plane of I vs G for three models
+% figure
+% hold on
+% plot(G, Ip, 'LineWidth', 1.5)
+% plot(GC, IpC, 'LineWidth', 1.5)
+% xlabel('[G] (mg/dl)')
+% ylabel('[I_p] (\muU/ml)')
+% legend('Original', 'New')
+% hold off
+% 
+% %% Periodogram
+% [freqSC, P1] = periods.ft_solution(tStC, yStC, 1440);
+% figure;
+% plot(freqSC, P1)
+% xlabel('Frequency (1/min)')
+% ylabel('Power')
+% xlim([0 0.02])
