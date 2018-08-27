90244cb53e771ca9e6f563071fbb7dcda38d466f
Ethan Ryan <ethanryan9@gmail.com>
2018-08-28 09:30:51 +1000

    Changed x ticks

--- +sims/circSolver.m
+++ +sims/circSolver.m
@@ -7,16 +7,16 @@ const = models.constants;
 
 %Change constants from default values
 %[const.times, const.Gin] = protocols.IdenticalMeals(50, nDays, deltaT);
-const.times = 0:deltaT:2*1440;
-const.Gin = protocols.saad12(deltaT);
+%const.times = 0:deltaT:2*1440;
+%const.Gin = protocols.saad12(deltaT);
 %%
-%const.Gin = 200;
+const.Gin = 243;
 
-const.g1 = 0.1;
+const.g1 = 0.3;
 const.phi1 = pi;
-const.g2 = 0.2;
+const.g2 = 0;
 const.phi2 = 0;
-const.g3 = 0.2;
+const.g3 = 0;
 const.phi3 = 0;
 %const.td = 5;
 %const.E = 0.2;
@@ -35,13 +35,13 @@ time = [0, 1440*nDays];
 %% Solve equations
 tSt = 0:1440*nDays; tStC = 0:1440*nDays;
 
-const.C1 = 1720; const.Rm = 150; const.a1 = 350;
+%const.C1 = 1720; const.Rm = 150; const.a1 = 350;
 %const.C5 = 18.6; const.Rg = 181.5; const.alpha = 0.1168;
-const.Vg = 13.3; const.Vi = 3.15; const.Vp = 5;
+%const.Vg = 13.3; const.Vi = 3.15; const.Vp = 5;
 
-ySt = utils.rk4Fixed(@models.sturisIK, sturisState, const, tSt);
+ySt = utils.rk4Fixed(@models.sturis, sturisState, const, tSt);
 
-yStC = utils.rk4Fixed(@models.sturisIKCirc, sturisState, const, tSt);
+yStC = utils.rk4Fixed(@models.sturisCirc, sturisState, const, tSt);
 
 
 %% Plotting
@@ -59,6 +59,9 @@ hold on
 plot(tSt/60, Ip)
 plot(tStC/60, IpC)
 hold off
+xlim([24 48])
+xticks(0:6:48)
+xticklabels(-24:6:24)
 ylabel('[I] (\muU/ml)')
 legend('Original', 'New')
 % Plot [G]
@@ -66,11 +69,14 @@ subplot(3,1,2)
 hold on
 plot(tSt/60, G)
 plot(tStC/60, GC)
+xlim([24 48])
 xticks(0:6:48)
-xticklabels(0:0.25:2)
+xticklabels(-24:6:24)
+
+%xticklabels(0:0.25:2)
 %plot([0 max(tT)/60], [mean(solLi.y(1,solLi.x>600)) mean(solLi.y(1,solLi.x>600))]/(10*const.Vg))
 hold off
-xlabel('Time (days)')
+xlabel('Clock Time')
 ylabel('[G] (mg/dl)')
 
 subplot(3,1,3)
@@ -82,7 +88,10 @@ catch
 end
 %plot([0 max(tT)/60], [mean(solLi.y(1,solLi.x>600)) mean(solLi.y(1,solLi.x>600))]/(10*const.Vg))
 hold off
-xlabel('Time (days)')
+xlim([1 2])
+xticks(1:0.25:2)
+xticklabels(0:6:24)
+xlabel('Clock Time')
 ylabel('G_{in} (mg/min)')
 
 %% Plot [G] and [I] (in % of baseline units) vs t for all 3 models.
@@ -95,8 +104,9 @@ plot(tStC/60,utils.meanPercent(IpC, 1440))
 hold off
 ylabel('[I] (% of mean)')
 legend('Original', 'New')
+xlim([24 48])
 xticks(0:6:48)
-xticklabels(0:0.25:2)
+xticklabels(-24:6:24)
 % Plot [G]
 subplot(3,1,2)
 hold on
@@ -106,8 +116,9 @@ plot(tStC/60, utils.meanPercent(GC, 1440))
 hold off
 xlabel('Time (days)')
 ylabel('[G] (% of mean)')
+xlim([24 48])
 xticks(0:6:48)
-xticklabels(0:0.25:2)
+xticklabels(-24:6:24)
 
 subplot(3,1,3)
 hold on
@@ -119,6 +130,8 @@ end
 
 %plot([0 max(tT)/60], [mean(solLi.y(1,solLi.x>600)) mean(solLi.y(1,solLi.x>600))]/(10*const.Vg))
 hold off
-
+xlim([1 2])
+xticks(1:0.25:2)
+xticklabels(0:6:24)
 xlabel('Time (days)')
 ylabel('G_{in} (mg/min)')
