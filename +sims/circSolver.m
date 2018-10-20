7567776019441c191805392c1b3faf8ccc6b18a3
ethrya <ethanryan9@gmail.com>
2018-10-20 15:49:11 +1100

    Updated plot formatting

--- +sims/circSolver.m
+++ +sims/circSolver.m
@@ -1,14 +1,17 @@
 clear;
 %% Preliminaries
-nDays = 4; % Simulation time (days)
+nDays = 5; % Simulation time (days)
 deltaT = 0.5; % Solver timestep (min)
 
 % Import constants class
 const = models.constants;
 % Change constants from default values
 const.Gin = 243;
-const.g1 = 0.25;
+const.g1 = .25;
 
+% Plot params
+nRows = 3; nCol = 1; N =1; tMin = 48;
+pltTitle = '';
 
 % Initial condition for Sturis and Tolic
 sturisState = [40; % Ip
@@ -38,7 +41,12 @@ G = ySt(:,3)/(const.Vg*10); %[G]=G/Vg mg/dl
 IpC = yStC(:,1)/const.Vp; %[I]=I/Vp microU/ml
 GC = yStC(:,3)/(const.Vg*10); %[G]=G/Vg mg/dl
 
-% Plot [G] and [I] vs t for all 3 models.
+% Calculate ISR
+ISR = models.funcs.f1(ySt(:,3), const);
+const.C = utils.skewSine(tStC/60)';
+ISR_circ = models.funcs.f1(yStC(:,3),const);
+
+%% Plot [G] and [I] vs t for all 3 models.
 figure()
 % Plot [I]
 subplot(3,1,1)
@@ -48,64 +56,74 @@ plot(tStC/60, IpC)
 hold off
 ylabel('[I] (\muU/ml)')
 legend('Original', 'New')
+xlim([tMin 24*nDays])
+xticks(0:6:nDays*24)
+xticklabels([])
 % Plot [G]
 subplot(3,1,2)
 hold on
 plot(tSt/60, G)
 plot(tStC/60, GC)
-xticks(0:6:48)
-xticklabels(0:0.25:2)
+xlim([tMin 24*nDays])
+xticks(0:6:nDays*24)
+xticklabels([])
 %plot([0 max(tT)/60], [mean(solLi.y(1,solLi.x>600)) mean(solLi.y(1,solLi.x>600))]/(10*const.Vg))
 hold off
-xlabel('Time (days)')
 ylabel('[G] (mg/dl)')
 
 subplot(3,1,3)
 hold on
 try
-    plot(const.times/1440,const.Gin)
+    plot(const.times/60,const.Gin)
 catch
-    plot(time/1440, [const.Gin const.Gin])
+    plot(time/60, [const.Gin const.Gin])
 end
 %plot([0 max(tT)/60], [mean(solLi.y(1,solLi.x>600)) mean(solLi.y(1,solLi.x>600))]/(10*const.Vg))
 hold off
-xlabel('Time (days)')
+xlim([tMin 24*nDays])
+xticks(0:6:nDays*24)
+xticklabels(0:6:nDays*24)
+xlabel('Time (h)')
 ylabel('G_{in} (mg/min)')
 
 %% Plot [G] and [I] (in % of baseline units) vs t for all 3 models.
 figure()
 % Plot [I]
-subplot(3,1,1)
+subplot(nRows,nCol,N)
 hold on
 plot(tSt/60, utils.meanPercent(Ip, 1440))
-plot(tStC/60,utils.meanPercent(IpC, 1440))
+plot(tStC/60,utils.meanPercent(IpC, 1440),'LineWidth',1)
+title(pltTitle)
 hold off
-ylabel('[I] (% of mean)')
+ylabel('[I] (%)')
 legend('Original', 'New')
-xticks(0:6:48)
-xticklabels(0:0.25:2)
+xlim([tMin 24*nDays])
+xticks(0:6:nDays*24)
+xticklabels([])
 % Plot [G]
-subplot(3,1,2)
+subplot(nRows,nCol,N+nCol)
 hold on
 plot(tSt/60, utils.meanPercent(G, 1440))
-plot(tStC/60, utils.meanPercent(GC, 1440))
+plot(tStC/60, utils.meanPercent(GC, 1440),'LineWidth',1)
 %plot([0 max(tT)/60], [mean(solLi.y(1,solLi.x>600)) mean(solLi.y(1,solLi.x>600))]/(10*const.Vg))
 hold off
-xlabel('Time (days)')
-ylabel('[G] (% of mean)')
-xticks(0:6:48)
-xticklabels(0:0.25:2)
-
-subplot(3,1,3)
+%xlabel('Time (days)')
+ylabel('[G] (%)')
+xlim([tMin 24*nDays])
+xticks(0:6:nDays*24)
+xticklabels([])
+subplot(nRows,nCol,N+2*nCol)
 hold on
-try
-    plot(const.times/1440,const.Gin)
-catch
-    plot(time/1440, [const.Gin const.Gin])
-end
+plot(tSt/60, utils.meanPercent(ISR,1440),'LineWidth',1)
+plot(tStC/60, utils.meanPercent(ISR_circ,1440),'LineWidth',1)
+xlabel('Time (hours)')
+ylabel('ISR (%)')
+xlim([tMin 24*nDays])
+xticks(0:6:nDays*24)
+xticklabels(0:6:nDays*24)
 
 %plot([0 max(tT)/60], [mean(solLi.y(1,solLi.x>600)) mean(solLi.y(1,solLi.x>600))]/(10*const.Vg))
 hold off
 
-xlabel('Time (days)')
-ylabel('G_{in} (mg/min)')
+%xlabel('Time (days)')
+ylabel('ISR (%)')
