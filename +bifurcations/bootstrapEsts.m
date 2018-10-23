d705ac983c9f7c8e5fec495016bb6ebf637e5234
ethrya <ethanryan9@gmail.com>
2018-10-24 09:54:16 +1100

    Added script to calculate bootstrap resamples

---
+++ +bifurcations/bootstrapEsts.m
@@ -0,0 +1,29 @@
+% Script to calculate bootstrap estimates of median and % samples <81mg/ml
+% and the associated uncertaintities
+clear var
+% Load .mat file that contains nx3 array of G samples from MC simulations
+% called (baseG)
+load('/import/suphys1/erya7975/simResults/2018-06-30/baselines')
+% Models in order of baseG
+model = ["Li", "Sturis", "Tolic"];
+% Number of boostrap resamples
+n = 100000;
+
+% Loop over each model
+for i=1:3
+    sprintf(model(i))
+    % Bootstrap sample of median. Use mean as best estimate and std as
+    % boostrap SE estimate
+    medSample = bootstrp(n, @(x) median(x), baseG(:,i)/100);
+    medMean = mean(medSample);
+    medSE = std(medSample);
+    sprintf('median +/- SE = %f +/- %f', medMean, medSE)
+    
+    % Bootstrap samples of % of population with [G]_b<81 mg/ml.
+    % Use mean as est of % and std of bootstap samples as SE
+    lowSample = bootstrp(n, @(x) sum(x<81), baseG(:,i)/100);
+    lowMean = mean(lowSample);
+    lowSE = std(lowSample);
+    sprintf('n<81 +/- SE = %f +/- %f',...
+            lowMean/length(baseG(:,i))*100, lowSE)
+end
